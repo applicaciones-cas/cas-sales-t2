8c1aa9c71936321d3e11c12b9a4588bf9645d81e
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2025-08-04 13:29:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cas-sales-t2
+
---
+++ src/main/java/ph/com/guanzongroup/cas/sales/t2/CasSalesT2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ph.com.guanzongroup.cas.sales.t2;
+
+/**
+ *
+ * @author Michael Cuison
+ */
+public class CasSalesT2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
